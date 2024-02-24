fc1036149872f4dc318502f070cb03beccdf832d
MadBowJam <madbowjam@gmail.com>
2024-02-24 23:55:48 +0200

    refactor code, added description in README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Steam Price
- 
-https://madbowjam.github.io/Steam-Price/
+https://madbowjam.github.io/Steam-Price/ <br>
+For make it work you need a Chrome extension to bypass CORS-policy. Link for doenload below: <br>
+https://chromewebstore.google.com/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,38 @@
-// To run it use next commands in CMD
-// cd c:\Program Files\Google\Chrome\Application
-// chrome.exe --disable-web-security --user-data-dir="c:/ChromeDevSession”
-// file:///D:/programming/SteamPrice/index.html;
-const link = 'https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=';
-document.getElementById('output').innerHTML += 'Приблизний час очікування — ' + Math.round((linkArray.length*3)/60 ) + ' хвилин' + "<br/>";
-let timer = 0;
+const link = 'https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name='; // Посилання на API
+document.getElementById('output').innerHTML += 'Приблизний час очікування — ' + Math.round((linkArray.length*5)/60 ) + ' хвилин' + "<br/>"; // Виводимо повідомлення
+let timer = 0; // Лічильник
 
-// Функція для форматування ціни з формату "$0.63" у "0,63"
-function formatPrice(priceString) {
-    // Відкидаємо символ "$" та замінюємо кому на крапку
-    return priceString.substring(1).replace('.', ',');
-}
+function fetchData() { // Функція для перевірки цін
+  const tableBody = document.getElementById('table'); // Отримуємо таблицю
+  const lowestPrices = []; // Оголошуємо масив для зберігання найнижчих цін
 
-function fetchData() {
-    const tableBody = document.getElementById('table');
-    // const data = []; // Масив для збереження отриманих даних
-    const lowestPrices = []; // Оголошуємо масив для зберігання найнижчих цін
+  const today = new Date().toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false }) // Отримуємо поточну дату та час
+    .replace(/\//g, '.') // Замінюємо "/" на "."
+    .replace(/[,\s:]/g, '_'); // Замінюємо пробіли, коми та крапки на "_"
 
-    // Отримання сьогоднішньої дати
-    const currentDate = new Date();
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // "+1" бо місяці в JS починаються з 0
-    const year = currentDate.getFullYear();
-    const hours = String(currentDate.getHours()).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-    const today = `${day}.${month}.${year}_(${hours}_${minutes})`;
+  for (let i = 0; i < linkArray.length; i++) { // Цикл для перевірки цін
+    setTimeout(() => { // Встановлення таймера
+      fetch(link + linkArray[i]) // Отримуємо дані з API
+      .then(response => { // Обробка відповіді
+          if (!response.ok) { // Перевірка статусу відповіді
+            throw new Error('Network response was not ok'); // Виводимо помилку
+          }
+          return response.json(); // Повертаємо дані відповіді
+      })
+      .then(response => { // Обробка відповіді
+        const formattedPrice = response.lowest_price.substring(1).replace('.', ','); // Форматування ціни
+        lowestPrices.push(formattedPrice); // Додавання форматованої ціни до масиву
+        tableBody.innerHTML += `<tr><th class="price">${formattedPrice}</th></tr>`; // Створення та додавання нового рядка для таблиці
 
-    for (let i = 0; i < linkArray.length; i++) {
-        setTimeout(() => {
-            fetch(link + linkArray[i])
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(response => {
-                    const price = response.lowest_price; // Отримання лише найнижчої ціни
-                    const formattedPrice = formatPrice(price); // Форматування ціни
-                    lowestPrices.push(formattedPrice); // Додавання форматованої ціни до масиву
-
-                    // Створення нового рядка для таблиці
-                    const newRow = document.createElement('tr');
-                    const newCell = document.createElement('th');
-                    newCell.classList.add('price');
-                    newCell.innerHTML = formattedPrice; // Встановлення форматованої ціни
-                    newRow.appendChild(newCell);
-                    tableBody.appendChild(newRow);
-
-                    // Перевірка, чи це останній запит
-                    if (lowestPrices.length === linkArray.length) {
-                        // Конвертування масиву з найнижчими цінами у рядок JSON
-                        const jsonData = JSON.stringify(lowestPrices);
-
-                        // Створення посилання для завантаження файлу JSON зі сьогоднішньою датою та часом в назві
-                        const blob = new Blob([jsonData], { type: 'application/json' });
-                        const url = URL.createObjectURL(blob);
-                        const link = document.createElement('a');
-                        link.href = url;
-                        link.download = `${today}.json`; // Використання сьогоднішньої дати та часу в назві файлу
-                        document.body.appendChild(link);
-                        link.click();
-                        document.body.removeChild(link);
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching data for', link + linkArray[i], ':', error);
-                    // Обробка помилки за необхідності
-                });
-        }, timer);
-        timer += 5000;
-    }
+        if (lowestPrices.length === linkArray.length) { // Перевірка, чи це останній запит
+          const url = URL.createObjectURL(new Blob([JSON.stringify(lowestPrices)], { type: 'application/json' })); // Створення посилання для завантаження файлу JSON зі сьогоднішньою датою та часом в назві | Конвертування масиву з найнижчими цінами у рядок JSON
+          (Object.assign(document.body.appendChild(document.createElement('a')), { href: url, download: `${today}.json` })).click(); // Використання сьогоднішньої дати та часу в назві файлу, додавання лінки на сторінку для завантаження JSON, та клік по ньому
+        }
+      })
+      .catch(error => { // Обробка помилки
+          console.error('Error fetching data for', link + linkArray[i], ':', error); // Обробка помилки при отриманні даних з API
+      });
+    }, timer); // Встановлення таймера
+    timer += 5000; // Збільшення таймера
+  }
 }
